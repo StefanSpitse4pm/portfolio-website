9598ef3360519388da54e99c132ebf59ebf5b755
StefanSpitse4pm <stefan.spitse@student.nhlstenden.com>
2026-05-24 23:28:24 +0200

    happy with structure for endpoint of file upload

--- backend/StefanSpitse/src/portfolio/router.py
+++ backend/StefanSpitse/src/portfolio/router.py
@@ -1,14 +1,21 @@
-from fastapi import APIRouter, Depends, UploadFile 
-from starlette.responses import Response
+from fastapi import APIRouter, Depends, HTTPException,UploadFile 
+import os
 
 from auth.dependencies import authenticate
 from auth.schemas import TokenData
+from portfolio.schemas import File
 
 router = APIRouter()
 
 @router.post("/portfolio/upload-pdf/")
-async def upload_pdf(uploaded_file: UploadFile,user: TokenData = Depends(authenticate)):
-    print(uploaded_file.file.name)
+async def upload_pdf(pdf: UploadFile,categories: list[str], user: TokenData = Depends(authenticate)):
+    if os.path.splitext(str(pdf.filename))[1] not in [".pdf", ".docx"]:
+        raise HTTPException(415)
+
+     
+
+        
+
 
 
 
