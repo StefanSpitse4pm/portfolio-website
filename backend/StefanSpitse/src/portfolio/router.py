af169884c34ac7760db414f3fd95dcc70456e7d4
StefanSpitse4pm <stefan.spitse@student.nhlstenden.com>
2026-05-25 02:40:36 +0200

    added enpoint to get all endpoint files

--- backend/StefanSpitse/src/portfolio/router.py
+++ backend/StefanSpitse/src/portfolio/router.py
@@ -3,7 +3,7 @@ import os
 import uuid
 import shutil
 from pathlib import Path
-from database import fetch_one, get_db_connection, execute
+from database import fetch_all, fetch_one, get_db_connection, execute
 from portfolio.service import does_category_exist
 
 from auth.dependencies import authenticate
@@ -33,8 +33,17 @@ async def upload_pdf(pdf: UploadFile, category:str | None = None, user: TokenDat
     
     await execute(Insert(Files).values(file_path=file_path, file_name=pdf.filename), db, commit=True) 
 
+    file = await fetch_one(Select(Files).where(Files.file_path == file_path), db)
     if category is not None:
         c = await does_category_exist(category, db) 
-        file = await fetch_one(Select(Files).where(Files.file_path == file_path), db)
         await execute(Insert(FileCategories).values(category_id=c["id"], file_id=file["id"]), db, commit=True)
+
+    return {"file_id": file["id"]}
+
+
+@router.get("/portfolio/files")
+async def get_all_files(user: TokenData = Depends(authenticate), db = Depends(get_db_connection)):
+    return await fetch_all(Select(Files), db)
+
+
 
